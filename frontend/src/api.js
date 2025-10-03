df388abb4847fd3f35117bf5428b90fc7d331c67
Bhuvanesh726 <bhuvaneshborra26@gmail.com>
2025-10-03 20:04:17 +0530

    V1.0.9

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,23 +1,20 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8081',
+  // CHANGED: Read the URL from the .env variable
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 // This interceptor will now correctly find and attach the JWT token
 api.interceptors.request.use(
   (config) => {
-    // 1. Look for the 'user' object in localStorage
     const userString = localStorage.getItem('user');
     
     if (userString) {
-      // 2. If it exists, parse the JSON to get the user object
       const userData = JSON.parse(userString);
-      // 3. Get the token from the user object
       const token = userData.token;
 
       if (token) {
-        // 4. If the token exists, add it to the Authorization header
         config.headers.Authorization = `Bearer ${token}`;
       }
     }
